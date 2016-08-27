e0d35383f06df7ad7340b59ded341178ce4c1f97
PaddyMurphy <patrickblewis@gmail.com>
2016-08-27 13:25:49 -0600

    fixed nav

--- src/js/main.js
+++ src/js/main.js
@@ -3,40 +3,42 @@ $(function() {
     var dataMountain = $('body').attr('data-mountain');
     var mountainList = ['bugaboo', 'tetons', 'rainier', 'glacier-peak'];
     var length = mountainList.length;
-    var title = $('.title');
-    var navRight = $('.nav-right');
-    var navLeft = $('.nav-left');
+
     var currentMountain, newMountain;
 
     $( "body" ).on( "click", ".nav-arrow", function(e) {
     	e.preventDefault();
     	currentMountain = mountainList.indexOf($('body').attr('data-mountain'));
-    	console.log('currentMountain: ' + currentMountain);
 
     	if(e.currentTarget.classList.contains('nav-right')) {
-    		// navigate( (currentMountain <= length) ? (currentMountain + 1) : 0 );
-    		newMountain = (currentMountain === length) ? 0 : (currentMountain + 1);
+    		// start at beginning again
+    		newMountain = (currentMountain === (length - 1)) ? 0 : (currentMountain + 1);
     		navigate(newMountain);
     		setTitle(mountainList[newMountain]);
     	} else {
-    		newMountain = (currentMountain <= length) ? (currentMountain - 1) : length;
+    		// start at end again
+    		newMountain = (currentMountain === 0) ? (length - 1) : (currentMountain - 1);
     		navigate(newMountain);
     		setTitle(mountainList[newMountain]);
     	}
     });
 
     function navigate(newMountain) {
-    	console.log('newMountain: ' + newMountain);
 		$('body').attr('data-mountain', mountainList[newMountain]);
+
     	console.log(currentMountain = mountainList.indexOf($('body').attr('data-mountain')));
     }
 
     function setTitle(title) {
-    	console.log(title);
-    	$('.title').html(title);
+    	$('.title').text(formatTitle(title));
+    }
+
+    function formatTitle(title) {
+    	return title = title.replace('-', ' ');
     }
 
     // function setMountain(mountain) {
+    // TODO: works once?
     // 	$('body').attr('data-mountain', mountain);
     // 	return false;
     // }
