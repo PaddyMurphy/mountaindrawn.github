4d4b0886cf7071b56676f3273a5df1c629759f1d
PaddyMurphy <patrickblewis@gmail.com>
2016-09-28 12:21:25 -0600

    riverflow fixes

--- src/js/riverflow.js
+++ src/js/riverflow.js
@@ -20,6 +20,9 @@
 }(this, function($) {
 
     function Riverflow(options) {
+        // var siteName, latitude, longitude, totalCount,
+        //     latestCfs, latestTime, mapURL;
+
         this.init();
         // options
         this.options = $.extend({}, this.constructor.defaults);
@@ -39,7 +42,14 @@
         flow5: 'The flow is moving now! Unless this is a large volume river like the Colorado or Rio Grande you can expect some really fast moving water.',
         flow6: 'DANGER! Possible death awaits! Unless this is a large volume river like the Colorado you may drown. Check with a local outfitter for more details on conditions before heading out.',
         graphPeriod: 7,
-        params: '&parameterCd=00060' // default river
+        params: '&parameterCd=00060', // default river
+        siteName: '',
+        latitude: '',
+        longitude: '',
+        totalCount: '',
+        latestCfs: '',
+        latestTime: '',
+        mapURL: ''
     };
 
     Riverflow.prototype.option = function(options) {
@@ -62,17 +72,20 @@
     	formatted = formatted.replace(/ /g, "").trim(); // replace spaces
     	formatted = formatted.replace(/(\r\n|\n|\r)/gm, ""); // remove line breaks
     	formatted = formatted.replace(/\-(\S*)\-/g, ""); // exclude titles (i.e. --brazosriverbasin--)
-        // console.log(formatted);
 
     	return formatted;
     };
 
     Riverflow.prototype.getUsgsData = function(river) {
     	var self = this;
-        var siteName, latitude, longitude, totalCount,
-        latestCfs, latestTime, mapURL;
 
-    	// fetches usgs instant data, usgs graph service, and flickr
+        var riverLocation = $('#selectRiver').val();
+
+        var usgsUrl = riverflow.options.baseURL +
+            riverLocation +
+            riverflow.options.params;
+
+    	// fetches usgs instant data, usgs graph service
     	// check if routed here
     	if (typeof (river) === 'string') {
     	    var options = document.querySelectorAll('#selectRiver option');
@@ -104,15 +117,7 @@
         // flowApp.config.loading.classList.remove("hidden");
         // flowApp.config.bodyTag.classList.add("loading");
 
-        var riverLocation = $('#selectRiver').val();
-
-        var pipeURL = riverflow.options.baseURL +
-            riverLocation +
-            riverflow.options.params;
-
-        console.log(pipeURL);
-
-        $.getJSON(pipeURL, function(data) {})
+        $.getJSON(usgsUrl, function(data) {})
             .done(function(data) {
                 // check if any data is returned
                 if (data.value.timeSeries.length === 0) {
@@ -123,63 +128,36 @@
                 } else {
                     $.each(data.value.timeSeries, function(i, item) {
                         // set the data variables for display
-                        siteName = item.sourceInfo.siteName;
-                        latitude = item.sourceInfo.geoLocation.geogLocation.latitude;
-                        longitude = item.sourceInfo.geoLocation.geogLocation.longitude;
-                        totalCount = item.values.count;
+                        riverflow.options.siteName = item.sourceInfo.siteName;
+                        riverflow.options.latitude = item.sourceInfo.geoLocation.geogLocation.latitude;
+                        riverflow.options.longitude = item.sourceInfo.geoLocation.geogLocation.longitude;
+                        riverflow.options.totalCount = item.values.count;
+                        latest = item.values[0].value.reverse()[0];
+
                         // set cfs value
-                        latestCfs = item.values[0].value[0].value;
+                        riverflow.options.latestCfs = latest.value;
                         // set date
-                        latestTime = item.values[0].value[0].dateTime;
+                        riverflow.options.latestTime = latest.dateTime;
                         // git array of values to chart
                         // NOTE: 7D data returns 2004 datapoints
                         //       see api to reduce # or filter out nth
                         // var graphData = item.values[0].value;
                         // self.displayChart(item.values[0].value);
-                        // console.log(item.values[0].value);
-                        console.log(latestCfs);
                     }); // END $.each
 
-                    // get todays date and trim hours
-                    var todaysDate = new Date();
-                    todaysDate = todaysDate.toDateString();
-
-                    // create latest cfs date object
-                    var d = new Date(latestTime);
-                    var timeDate = d.toDateString();
-                    var timeHours = d.toLocaleTimeString();
-
-                    // compare todays date with the latest returned time
-                    // TODO: this logic is flawed, should return time and format from localStorage
-                    // if (todaysDate === timeDate) {
-                    //     timeDate = 'Today';
-                    // }
-
-                    latestTime = timeDate + ' at ' + timeHours;
-
-                    // save the name,cfs and display
-                    var recentInfo = '<div class="recentValue">' + latestCfs +
-                        '<abbr class="cfs" title="cubic feet per second">cfs</abbr> <span class="name">' +
-                        siteName + '</span><span class="latestTime"> ' +
-                        latestTime + '</span>' + '</div>';
-
-                    // TODO: latest info is disabled
-                    //       fix ux and storage issue (does not delete)
-                    // flowApp.saveLatestCfs(recentInfo);
-
                     // create map link
-                    mapURL = riverflow.options.baseMapURL + latitude + ',+' + longitude;
+                    riverflow.options.mapURL = riverflow.options.baseMapURL + riverflow.options.latitude + ',+' + riverflow.options.longitude;
                     // TODO: round decimal and show the flow conditions message
                     // self.displayConditions(parseInt(self.latestCfs, 10));
                     // TODO: display the data
-                    // self.displayData();
+                    riverflow.displayData();
 
                 } // END check if any data is returned
 
                 // debug - show all data
-                console.log(data.value);
-                document.getElementsByTagName('body')[0].className = '';
-
+                // console.log(data.value);
+                // TODO: what was this for? can't wipe out body class
+                // document.getElementsByTagName('body')[0].className = '';
                 // flowApp.config.loading.classList.add('hidden');
 
             })
@@ -190,6 +168,21 @@
 
         return false; // prevent click
     };
+
+    Riverflow.prototype.displayData = function() {
+        // display tiles: siteName, flowRate, mapLinkLatLong
+        var flowrate = document.querySelector('.flowrate');
+
+        // mapLinkLatLong
+        var mapLinkLatLong = '<a href="' + this.options.mapURL + '">' + 'View a Map' + '</a>';
+        mapLinkLatLong += 'Latitude: ' + this.options.latitude + ' &nbsp; Longitude: ' + this.options.longitude;
+
+        //flowrate
+        var flowrateText = '<h2>' + this.options.latestCfs + '<abbr id="flowCfs" title="cubic feet per second">CFS</abbr>' + '</h2>';
+        flowrateText += '<div class="mapLinkLatLong">' + mapLinkLatLong + '</div>';
+
+        flowrate.innerHTML = flowrateText;
+    }
 
     return new Riverflow();
 }));
